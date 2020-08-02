e9bce7934fc63b20e6c5e266ebc9879bcb8f199a
Bernardo <behornerl@gmail.com>
2020-08-02 14:49:34 -0300

    Criando script que controla o movimento de personagens

---
+++ Artisan/Assets/Scripts/Controls/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private GroundedChecker groundedChecker;
+
+    private void Start()
+    {
+        moveSpeed *= 100;
+    }
+
+    private float downwardSpeed = 0;
+    public void Move(float horizontal, float vertical)
+    {
+        if (groundedChecker.GetIsGrounded())
+        {
+            downwardSpeed = 0;
+            rigidbody.velocity = new Vector3(horizontal, 0, vertical).normalized * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            downwardSpeed += gravity;
+            rigidbody.velocity = new Vector3(horizontal, -downwardSpeed, vertical).normalized * moveSpeed * Time.deltaTime;
+        }
+    }
+}
