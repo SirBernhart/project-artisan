536fe47cda99ac0b5a776ab89033de8668d4b72e
Bernardo <behornerl@gmail.com>
2020-08-02 14:49:03 -0300

    Criando script que checa se o personagem está pisando em algo sólido

---
+++ Artisan/Assets/Scripts/Controls/GroundedChecker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+class GroundedChecker : MonoBehaviour
+{
+    private bool isGrounded;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != this.transform.root.tag)
+            isGrounded = true;
+        else
+            isGrounded = false;
+    }
+
+    public bool GetIsGrounded()
+    {
+        return isGrounded;
+    }
+}
+
