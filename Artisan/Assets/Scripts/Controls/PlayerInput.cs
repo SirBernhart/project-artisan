9c03969c61afc2b462087c0ed80490668197006d
Bernardo <behornerl@gmail.com>
2020-08-02 14:49:50 -0300

    Criando script que capta o input do jogador

---
+++ Artisan/Assets/Scripts/Controls/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+class PlayerInput : MonoBehaviour
+{
+    float vertical, horizontal;
+    [SerializeField] Movement playerMovement;
+
+    private void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        playerMovement.Move(horizontal, vertical);
+    }
+}
