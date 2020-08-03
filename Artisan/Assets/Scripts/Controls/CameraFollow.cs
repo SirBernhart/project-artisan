e3b2c9c2601c6b3547c2a8619fda8f82b6b0fbeb
Bernardo <behornerl@gmail.com>
2020-08-02 23:42:29 -0300

    Criando script da camera seguir o jogador

---
+++ Artisan/Assets/Scripts/Controls/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 cameraOffset;
+    [SerializeField] private Transform target;
+
+    void Update()
+    {
+         transform.position = target.position + cameraOffset;    
+    }
+}
