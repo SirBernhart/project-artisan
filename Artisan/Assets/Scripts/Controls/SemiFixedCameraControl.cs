9649e7e15f0b1de37d1c06f1898480bac62e444c
Bernardo <behornerl@gmail.com>
2020-08-15 17:18:45 -0300

    Adicionando controle de camera semi-fixa (rotaciona quantidades fixas)

---
+++ Artisan/Assets/Scripts/Controls/SemiFixedCameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SemiFixedCameraControl : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float distance;
+    private bool isTurning;
+
+    public void TurnCamera(bool turnRight)
+    {
+        if(turnRight)
+            this.transform.RotateAround(target.position, Vector3.up, 45);
+        else
+            this.transform.RotateAround(target.position, Vector3.up, -45);
+    }
+}
