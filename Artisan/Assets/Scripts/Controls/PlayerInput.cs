e089bb98ce49c4b42b82f155853aa9d96f33e629
Bernardo <behornerl@gmail.com>
2020-08-15 17:19:08 -0300

    Adicionando controle da camera semi-fixa no input do jogador

--- Artisan/Assets/Scripts/Controls/PlayerInput.cs
+++ Artisan/Assets/Scripts/Controls/PlayerInput.cs
@@ -4,12 +4,24 @@ class PlayerInput : MonoBehaviour
 {
     float vertical, horizontal;
     [SerializeField] Movement playerMovement;
+    [SerializeField] SemiFixedCameraControl semiFixedCamera;
 
     private void Update()
     {
         vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        if(semiFixedCamera != null)
+        {
+            float rotateDirection = Input.GetKeyDown("Rotate camera");
+            if(rotateDirection > 0)
+                semiFixedCamera.TurnCamera(true);
+            else
+            {
+                semiFixedCamera.TurnCamera(true);
+
+            }
+        }
     }
 
     private void FixedUpdate()
