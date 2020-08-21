b3e038a2b4cebc350067d6dbf168a2c5666aa2d7
Bernardo <behornerl@gmail.com>
2020-08-20 22:58:49 -0300

    Criando script de Scriptable Object para os efeitos dos ingredientes

---
+++ Artisan/Assets/Scripts/Crafting/Item/IngredientEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public enum EffectType { Envigorate, Resist, Heal, Poison, Heat, Cold, Light }
+public enum RefinementType { Mascerate, Toast, Distilation, Charring, Powdering, Drying, Decantation, Combination }
+
+public class IngredientEffect : ScriptableObject
+{
+    public List<EffectType> possibleEffects = new List<EffectType>();
+    public List<RefinementType> requiredRefinements = new List<RefinementType>();
+
+    public Dictionary<RefinementType, EffectType> effectsAndRequirementsDictionary = new Dictionary<RefinementType, EffectType>();
+
+    public EffectType GetResultingEffect(RefinementType refinement)
+    {
+        EffectType resultingEffect;
+        effectsAndRequirementsDictionary.TryGetValue(refinement, out resultingEffect);
+
+        return resultingEffect;
+    }
+}
+
