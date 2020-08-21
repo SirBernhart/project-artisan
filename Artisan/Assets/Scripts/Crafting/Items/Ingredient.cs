a757544e447d6e351771a3592727965a47a3d66a
Bernardo <behornerl@gmail.com>
2020-08-21 02:18:44 -0300

    Mudando os scripts de arquivo

---
+++ Artisan/Assets/Scripts/Crafting/Items/Ingredient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    [SerializeField] private IngredientEffect possibleEffects;
+    private IngredientEffectInitializer ingredientEffectInitializer;
+
+    private EffectType activatedEffect;
+    private bool isRefined;
+
+    private void Start()
+    {
+        ingredientEffectInitializer = new IngredientEffectInitializer(possibleEffects);
+    }
+
+    public EffectType GetResultingEffect(RefinementType refinementType)
+    {
+        return possibleEffects.GetResultingEffect(refinementType);
+    }
+
+    public void SetActivatedEffect(EffectType value)
+    {
+        activatedEffect = value;
+    }
+
+    public EffectType GetActivatedEffect()
+    {
+        return activatedEffect;
+    }
+
+    public void SetIsRefined(bool value)
+    {
+        isRefined = value;
+    }
+
+    public bool GetIsRefined()
+    {
+        return isRefined;
+    }
+}
