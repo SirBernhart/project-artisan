94383a7406c516baf5e14dc200c72c1f18649a1e
Bernardo <behornerl@gmail.com>
2020-08-21 02:20:19 -0300

    Criando script para armazenar os itens nas crafting stations

---
+++ Artisan/Assets/Scripts/Crafting/Crafting Stations/IngredientSlot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSlot : MonoBehaviour
+{
+    private Ingredient slottedIngredient;
+
+    public Ingredient GetSlottedIngredient()
+    {
+        return slottedIngredient;
+    }
+
+    public bool TryAddIngredient(Ingredient ingredient)
+    {
+        if(slottedIngredient == null)
+        {
+            slottedIngredient = ingredient;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
