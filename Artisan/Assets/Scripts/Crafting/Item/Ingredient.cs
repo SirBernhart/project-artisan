97fd8d3dba6420c45dff836e5db3c16b5102e148
Bernardo <behornerl@gmail.com>
2020-08-20 22:59:11 -0300

    Criando script para os ingredientes

---
+++ Artisan/Assets/Scripts/Crafting/Item/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    [SerializeField] private IngredientEffect possibleEffects;
+    private IngredientEffectInitializer ingredientEffectInitializer;
+
+    private void Start()
+    {
+        ingredientEffectInitializer = new IngredientEffectInitializer(possibleEffects);
+    }
+}
