bce0a12204fbdb14ac04f9257d654d8b29c52335
Bernardo <behornerl@gmail.com>
2020-08-20 23:00:09 -0300

    Criando classe auxiliar para preencher o Dictionary do scriptable object IngredientEffect

---
+++ Artisan/Assets/Scripts/Crafting/Item/IngredientEffectInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+class IngredientEffectInitializer
+{
+    public IngredientEffectInitializer(IngredientEffect ingredientEffect)
+    {
+        List<EffectType> possibleEffects = ingredientEffect.possibleEffects;
+        List<RefinementType> requiredRefinements = ingredientEffect.requiredRefinements;
+
+        if (possibleEffects.Count != requiredRefinements.Count)
+        {
+            Debug.LogError("Number of effects differ from the number of required refinement");
+        }
+        else
+        {
+            for (int i = 0 ; i<possibleEffects.Count ; ++i)
+            {
+                ingredientEffect.effectsAndRequirementsDictionary.Add(requiredRefinements[i], possibleEffects[i]);
+            }
+        }
+    }
+
+}
