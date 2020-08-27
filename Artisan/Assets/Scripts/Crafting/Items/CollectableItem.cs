d27fb2dd19e3673f4bc977d52eeba6c5b7c65daa
Bernardo <behornerl@gmail.com>
2020-08-27 01:16:58 -0300

    Criando script que que permite um item ser coletado

---
+++ Artisan/Assets/Scripts/Crafting/Items/CollectableItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour
+{
+    [SerializeField] private Item itemScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ItemCollector itemCollector = other.gameObject.GetComponent<ItemCollector>();
+        if(itemCollector != null)
+        {
+            itemCollector.AddItemInRange(itemScript);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ItemCollector itemCollector = other.gameObject.GetComponent<ItemCollector>();
+
+        if(itemCollector != null)
+        {
+            itemCollector.RemoveItemInRange(itemScript);
+        }
+    }
+}
