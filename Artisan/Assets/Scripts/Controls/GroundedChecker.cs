033109cdb8e535daba85d6858343bb8e65b4f0a0
Bernardo <behornerl@gmail.com>
2020-08-27 01:20:51 -0300

    Fazendo com que o jogador caia

--- Artisan/Assets/Scripts/Controls/GroundedChecker.cs
+++ Artisan/Assets/Scripts/Controls/GroundedChecker.cs
@@ -4,12 +4,17 @@ class GroundedChecker : MonoBehaviour
 {
     private bool isGrounded;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag != this.transform.root.tag)
             isGrounded = true;
         else
             isGrounded = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
     }
 
     public bool GetIsGrounded()
