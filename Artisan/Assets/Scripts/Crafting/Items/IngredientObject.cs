b08441ee1aefd127fba050e9d7bba4b3877dfc03
Bernardo <behornerl@gmail.com>
2020-08-27 01:07:18 -0300

    Criando scriptable object para os ingredientes

---
+++ Artisan/Assets/Scripts/Crafting/Items/IngredientObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Ingredient Object", menuName = "Scriptable Objects/Items/Ingredient")]
+public class IngredientObject : ItemObject
+{
+    private void Awake()
+    {
+        base.type = ItemType.Ingredient;
+    }
+}
