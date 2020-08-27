40057e8660be861dc824689f2c3e5850874534d2
Bernardo <behornerl@gmail.com>
2020-08-27 01:07:35 -0300

    Criando scriptable object para itens do tipo Produto

---
+++ Artisan/Assets/Scripts/Crafting/Items/ProductObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Product Object", menuName = "Scriptable Objects/Items/Product")]
+public class ProductObject : ItemObject
+{
+    private void Awake()
+    {
+        base.type = ItemType.Product;
+    }
+}
