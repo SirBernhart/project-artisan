96397df50326fd0062baf07d64c62afc71203915
Bernardo <behornerl@gmail.com>
2020-08-27 01:17:18 -0300

    Criando um item que permite o jogador a coletar itens

---
+++ Artisan/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    [SerializeField] private Inventory inventory;
+    private List<Item> itensInRange = new List<Item>();
+
+    public void CollectItem()
+    {
+        Item itemToCollect = GetNearestItem();
+
+        if(itemToCollect != null)
+        {
+            inventory.TryAddItem(itemToCollect.item, 1);
+        }
+    }
+
+    private Item GetNearestItem()
+    {
+        if(itensInRange.Count > 0)
+        {
+            Item nearestItem;
+            nearestItem = itensInRange[0];
+            foreach(Item item in itensInRange)
+            {
+                if (Vector3.Distance(item.transform.position, this.transform.position) < 
+                    Vector3.Distance(nearestItem.transform.position, this.transform.position))
+                {
+                    nearestItem = item;
+                }
+            }
+            return nearestItem;
+        }
+        return null;
+    }
+
+    public void AddItemInRange(Item item)
+    {
+        itensInRange.Add(item);
+    }
+
+    public void RemoveItemInRange(Item item)
+    {
+        itensInRange.Remove(item);
+    }
+}
