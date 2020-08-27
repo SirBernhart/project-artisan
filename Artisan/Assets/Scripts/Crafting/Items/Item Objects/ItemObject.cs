90443f81309a6cca1635ea387eb6ee9515168d79
Bernardo <behornerl@gmail.com>
2020-08-27 01:06:55 -0300

    Criando scriptable object base para os scriptable objects de itens

---
+++ Artisan/Assets/Scripts/Crafting/Items/Item Objects/ItemObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Ingredient,
+    Product
+}
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public Sprite UISprite;
+    public ItemType type;
+    [TextArea(15, 20)]
+    public string description;
+}
